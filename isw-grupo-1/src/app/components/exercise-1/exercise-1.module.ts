ae41ab11f115632a33422f851136ad2aad26a9e8
Gabriel Zitelli <gabriel.luciano.zitelli@gmail.com>
2023-09-10 18:14:54 -0300

    feat: create index to initialize data when module is loaded

--- isw-grupo-1/src/app/components/exercise-1/exercise-1.module.ts
+++ isw-grupo-1/src/app/components/exercise-1/exercise-1.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/app/shared';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import * as fromExercise1Store from './store';
 
 @NgModule({
   declarations: [
@@ -11,8 +12,7 @@ import { EffectsModule } from '@ngrx/effects';
   imports: [
     CommonModule,
     SharedModule,
-    StoreModule,
-    EffectsModule
+    StoreModule.forFeature(fromExercise1Store.exercise1FeatureKey, fromExercise1Store.reducers, { metaReducers: [] })
   ]
 })
 export class ExerciseOneModule {}
---
+++ isw-grupo-1/src/app/components/exercise-1/store/index.ts
@@ -0,0 +1,27 @@
+import { ActionReducerMap, createFeatureSelector } from '@ngrx/store';
+
+import * as fromBusinessParts from './reducers/business-adress.reducer';
+import * as fromCitysParts from './reducers/citys.reducer';
+import * as fromCustomerAdressParts from './reducers/customer-adress.reducer';
+import * as fromPaymentParts from './reducers/payment.reducer';
+import * as fromProductDataParts from './reducers/product-data.reducer';
+
+export const exercise1FeatureKey = 'exercise1';
+
+export interface Exercise1State {
+  [fromBusinessParts.businessAdressFeatureKey]: fromBusinessParts.State;
+  [fromCitysParts.cityFeatureKey]: fromCitysParts.State;
+  [fromCustomerAdressParts.customerAdressFeatureKey]: fromCustomerAdressParts.State;
+  [fromPaymentParts.paymentFeatureKey]: fromPaymentParts.State;
+  [fromProductDataParts.productDataFeatureKey]: fromProductDataParts.State;
+}
+
+export const reducers: ActionReducerMap<Exercise1State> = {
+  [fromBusinessParts.businessAdressFeatureKey]: fromBusinessParts.reducer,
+  [fromCitysParts.cityFeatureKey]: fromCitysParts.reducer,
+  [fromCustomerAdressParts.customerAdressFeatureKey]: fromCustomerAdressParts.reducer,
+  [fromPaymentParts.paymentFeatureKey]: fromPaymentParts.reducer,
+  [fromProductDataParts.productDataFeatureKey]: fromProductDataParts.reducer
+};
+
+export const selectTransactionFeatureState = createFeatureSelector<Exercise1State>(exercise1FeatureKey);
