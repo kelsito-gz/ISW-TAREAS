50f242ad95e599b9352f3f1d59ae1064405fa2d7
Gabriel Zitelli <gabriel.luciano.zitelli@gmail.com>
2023-09-11 16:17:11 -0300

    feat: create customer adress component

--- isw-grupo-1/src/app/components/exercise-1/components/customer-adress/customer-adress.component.ts
+++ isw-grupo-1/src/app/components/exercise-1/components/customer-adress/customer-adress.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, Input } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { City } from '../../models';
 
 @Component({
   selector: 'app-customer-adress',
@@ -9,15 +10,41 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class CustomerAdressComponent {
   constructor(private fb: FormBuilder) {}
 
+  @Input() citys: City[];
+
   form: FormGroup;
 
   ngOnInit(): void {
-
+    this.initForm();
   }
 
   initForm() {
     this.form = this.fb.group({
-      
+      adress: [ '', [ Validators.required ] ],
+      number: [ '', [ Validators.required ] ],
+      city: [ '', [ Validators.required ] ],
+      asSoonAsItPosible: [ false ],
+      date: [ '', [ Validators.required ]]
     })
+  }
+
+  onSoonChange() {
+    this.date.reset();
+    if (this.asSoonAsItPosible.value) {
+      this.date.clearValidators();
+      this.date.disable();
+    } else {
+      this.date.setValidators([Validators.required]);
+      this.date.enable();
+    }
+    this.date.updateValueAndValidity();
+  }
+
+  get asSoonAsItPosible(): AbstractControl {
+    return this.form.get('asSoonAsItPosible');
+  }
+
+  get date(): AbstractControl {
+    return this.form.get('date');
   }
 }
