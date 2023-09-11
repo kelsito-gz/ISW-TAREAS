918a9c66172a62e8a95ac23916512709f797985a
Gabriel Zitelli <gabriel.luciano.zitelli@gmail.com>
2023-09-11 12:59:46 -0300

    feat: redirect in menu

--- isw-grupo-1/src/app/components/exercise-1/components/menu/menu.component.ts
+++ isw-grupo-1/src/app/components/exercise-1/components/menu/menu.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'exerice-one-menu-component',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+  constructor(private router: Router) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  redirectBusinessAdress() {
+    this.router.navigate([ 'delivereat/local' ]);
+  }
+
+  redirectCustomerAdress() {
+    this.router.navigate([ 'delivereat/domicilio' ]);
+  }
+
+  redirectPayment() {
+    this.router.navigate([ 'delivereat/pago' ]);
+  }
+
+  redirectProductData() {
+    this.router.navigate([ 'delivereat/producto' ]);
+  }
 
 }
--- isw-grupo-1/src/app/components/exercise-1/exercise-1-routing.module.ts
+++ isw-grupo-1/src/app/components/exercise-1/exercise-1-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MenuContainer } from './containers/menu/menu.container';
+import { ProductDataContainer } from './containers/product-data/product-data.container';
+import { BusinessAdressContainer } from './containers/business-adress/business-adress.container';
+import { CustomerAdressContainer } from './containers/customer-adress/customer-adress.container';
+import { PaymentContainer } from './containers/payment/payment.container';
 
 const routes: Routes = [
-  { path: '', component: MenuContainer }
+  { path: '', component: MenuContainer },
+  { path: 'producto', component:  ProductDataContainer},
+  { path: 'local', component:  BusinessAdressContainer},
+  { path: 'domicilio', component:  CustomerAdressContainer},
+  { path: 'pago', component:  PaymentContainer},
 ];
 
 @NgModule({
